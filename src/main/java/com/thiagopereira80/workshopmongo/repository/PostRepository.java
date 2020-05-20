b39689a2bf926debdcfebc202d9e58a8f2a56297
ThiagoPereira80 <thiagoalmeidasp@hotmail.com>
2020-05-20 17:36:53 -0300

    Criando Entity Post com user aninhado

---
+++ src/main/java/com/thiagopereira80/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.thiagopereira80.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thiagopereira80.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
